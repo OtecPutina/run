e2c85f5bb09cd71961d9db82240df13899915426
OtecPutina <116644378+OtecPutina@users.noreply.github.com>
2024-09-03 11:46:48 +0400

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,18 @@
+"""Authentication credentials used by the bot."""
+
+# FIXME: move credentials out of file.
+
+# Reddit Praw
+praw_client_id = "ggqd1HLeCqjIrP9CZiysmQ"  # noqa: S105
+praw_client_secret = "	pItw6bdEY75lk5eeHe7uXklvii1QBA"  # noqa: S105
+praw_user_agent = "Jefraww_App"
+praw_username = "xxxxxx"
+praw_password = "xxxxxx"  # noqa: S105
+
+# Amazon Polly
+aws_access_key_id = "xxxxxx"
+aws_secret_access_key = "xxxxxx"  # noqa: S105
+
+# Rumble
+rumble_username = "xxxxxx"
+rumble_password = "xxxxxx"  # noqa: S105
